bd5f5288b725267623ce4f68db0aa60dacc8d408
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-06-15 19:11:49 +0300

    dw

---
+++ script.js
@@ -0,0 +1,15 @@
+const lazyImages = document.querySelectorAll('.lazy-img');
+
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.add('loaded');
+            observer.unobserve(img);
+          }
+        });
+      });
+
+
+      lazyImages.forEach(img => observer.observe(img));
